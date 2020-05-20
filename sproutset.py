c520eb287a711e2343250e2433606dcc27a6a271
terHahaha <64753139+terHahaha@users.noreply.github.com>
2020-05-20 17:56:02 +0100

    Game settings

---
+++ sproutset.py
@@ -0,0 +1,15 @@
+class Settings():
+    def __init__(self):
+
+            #Dimensions
+            self.width = 1000
+            self.height = 600
+
+            #Colours
+            self.RED = (255, 0, 0)
+            self.BLACK = (0, 0, 0)
+            self.BLUE = (0, 0, 255)
+            self.WHITE = (255, 255, 255)
+            self.ORANGE = (255,165,0)
+
+            
